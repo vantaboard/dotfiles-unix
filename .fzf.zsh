efff6a87f07f951aadf1642a8fa5c42db5adee15
Brighten Tompkins <brightenqtompkins@gmail.com>
2022-12-28 17:01:19 -0800

    :heavy_plus_sign: (zsh) plugins baby!

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,9 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */home/blackboardd/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/blackboardd/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/blackboardd/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/blackboardd/.fzf/shell/key-bindings.zsh"
