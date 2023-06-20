fd9d24fe6491dfb6593cfd3b10656fdbf0645217
brighten <brightenqtompkins@gmail.com>
2023-06-20 11:58:13 -0700

    autosave reliable debounce

--- .config/nvim/lua/plugins/autosave.lua
+++ .config/nvim/lua/plugins/autosave.lua
@@ -6,7 +6,7 @@ require'auto-save'.setup(
 			return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
 		end,
 		dim = 0.18, -- dim the color of `message`
-		cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+		cleaning_interval = 0, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
 	},
     trigger_events = {"InsertLeave", "TextChanged"}, -- vim events that trigger auto-save. See :h events
 	-- function that determines whether to save the current buffer or not
@@ -24,7 +24,7 @@ require'auto-save'.setup(
 		return false -- can't save
 	end,
     write_all_buffers = false, -- write all buffers when the current one meets `condition`
-    debounce_delay = 500, -- saves the file at most every `debounce_delay` milliseconds
+    debounce_delay = 1000, -- saves the file at most every `debounce_delay` milliseconds
 	callbacks = { -- functions to be executed at different intervals
 		enabling = nil, -- ran when enabling auto-save
 		disabling = nil, -- ran when disabling auto-save
