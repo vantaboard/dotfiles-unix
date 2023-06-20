66989abf80e76e6c92539233487eef5e6af342bf
brighten <brightenqtompkins@gmail.com>
2023-06-20 11:59:01 -0700

    treesitter dont permanently trash my stuff man

--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -1,4 +1,19 @@
-require'nvim-tree'.setup {}
+require 'nvim-tree'.setup({
+    trash = {
+        cmd = "gio trash",
+        require_confirm = false,
+    },
+    on_attach = function(bufnr)
+        local api = require('nvim-tree.api')
+
+        local function opts(desc)
+            return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+        api.config.mappings.default_on_attach(bufnr)
+
+        vim.keymap.set('n', 'd', api.fs.trash, opts('Trash'))
+    end,
+})
 
 local optsrsw = { noremap = true, silent = true, nowait = true }
 
