39b2f9a9b84900d535ed69c1a52411e30f0707e2
brighten <brightenqtompkins@gmail.com>
2023-06-21 16:41:40 -0700

    :fire: (neovim) clean up configs

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -11,17 +11,3 @@ vim.api.nvim_create_autocmd('BufWrite', {
 vim.cmd [[
 autocmd BufRead,BufNewFile tsconfig.json set filetype=jsonc
 ]]
-
---
--- local group = vim.api.nvim_create_augroup("jump_last_position", { clear = true })
--- vim.api.nvim_create_autocmd(
--- 	"BufReadPost",
--- 	{callback = function()
--- 			local row, col = unpack(vim.api.nvim_buf_get_mark(0, "\""))
--- 			if {row, col} ~= {0, 0} then
--- 				vim.api.nvim_win_set_cursor(0, {row, 0})
--- 			end
--- 		end,
--- 	group = group
--- 	}
--- )
