114da763824ca5621d2dbc4cfb9ec46c5e57205e
brighten <brightenqtompkins@gmail.com>
2023-07-22 15:36:51 -0700

    extra telescope pickers and mappings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,9 @@
+require("textcase").setup({})
+
 local open_in_nvim_tree = function(prompt_bufnr)
-    local action_state = require "telescope.actions.state"
-    local Path = require "plenary.path"
-    local actions = require "telescope.actions"
+    local action_state = require("telescope.actions.state")
+    local Path = require("plenary.path")
+    local actions = require("telescope.actions")
 
     local entry = action_state.get_selected_entry()[1]
     local entry_path = Path:new(entry):parent():absolute()
@@ -12,7 +14,7 @@ local open_in_nvim_tree = function(prompt_bufnr)
     vim.cmd("NvimTreeClose")
     vim.cmd("NvimTreeOpen " .. entry_path)
 
-    file_name = nil
+    local file_name = nil
     for s in string.gmatch(entry, "[^/]+") do
         file_name = s
     end
@@ -20,23 +22,59 @@ local open_in_nvim_tree = function(prompt_bufnr)
     vim.cmd("/" .. file_name)
 end
 
-actions = require("telescope.actions")
+local utils = require("yanky.utils")
+local mapping = require("yanky.telescope.mapping")
+require("yanky").setup({
+    preserve_cursor_position = {
+        enabled = true,
+    },
+    highlight = {
+        on_put = false,
+        on_yank = false,
+    },
+    picker = {
+        telescope = {
+            mappings = {
+                default = mapping.put("p"),
+                i = {
+                    -- make return key add to default register
+                    ["<cr>"] = mapping.set_register(
+                        utils.get_default_register()
+                    ),
+                    ["<c-s>"] = mapping.set_register,
+                    ["<c-x>"] = mapping.delete(),
+                },
+                n = {
+                    p = mapping.put("p"),
+                    P = mapping.put("P"),
+                    d = mapping.delete(),
+                    r = mapping.set_register(utils.get_default_register()),
+                },
+            },
+        },
+    },
+})
+
+local actions = require("telescope.actions")
 require("telescope").load_extension("ui-select")
-require('telescope').load_extension('fzf')
+require("telescope").load_extension("fzf")
 require("telescope").load_extension("harpoon")
-require('telescope').load_extension('dap')
+require("telescope").load_extension("dap")
+require("telescope").load_extension("yank_history")
+require("telescope").load_extension("undo")
+require("telescope").load_extension("textcase")
 
-require('textcase').setup {}
-require('telescope').load_extension('textcase')
-vim.api.nvim_set_keymap('n', 'ga.', '<cmd>TextCaseOpenTelescope<CR>', { desc = "Telescope" })
-vim.api.nvim_set_keymap('v', 'ga.', "<cmd>TextCaseOpenTelescope<CR>", { desc = "Telescope" })
-
-require("telescope").setup {
+require("telescope").setup({
     defaults = {
         mappings = {
             i = {
+                ["C-Q"] = actions.send_selected_to_qflist + actions.open_qflist,
+                ["<C-u>"] = false,
+                ["<C-j>"] = actions.cycle_history_next,
+                ["<C-k>"] = actions.cycle_history_prev,
                 ["<c-s>"] = open_in_nvim_tree,
-                ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                ["<C-w>"] = actions.send_selected_to_qflist
+                    + actions.open_qflist,
             },
             n = {
                 ["<c-s>"] = open_in_nvim_tree,
@@ -45,11 +83,11 @@ require("telescope").setup {
     },
     extensions = {
         ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
+            require("telescope.themes").get_dropdown({}),
         },
         undo = {
             use_delta = true,
-            use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
+            use_custom_command = nil,
             side_by_side = false,
             diff_context_lines = vim.o.scrolloff,
             entry_format = "state #$ID, $STAT, $TIME",
@@ -61,24 +99,64 @@ require("telescope").setup {
             },
         },
         fzf = {
-            fuzzy = true,                   -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true,    -- override the file sorter
-            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
-            -- the default case_mode is "smart_case"
-        }
-    }
-}
-
-require("telescope").load_extension("undo")
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+        },
+    },
+})
 
 -- disable backups
-vim.cmd([[
-    set noswapfile
-    set nobackup
-    set undofile
-    set undodir=~/.vim/undo
-]])
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undofile = true
+vim.o.undodir = vim.fn.expand("~/.vim/undo")
 
-local optsrws = { noremap = true, silent = true, nowait = true }
-vim.keymap.set("n", "<leader>U", "<cmd>Telescope undo<cr>")
+---------- mappings ----------
+-- built ins
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", function()
+    builtin.find_files({ hidden = true })
+end)
+vim.keymap.set("n", "<leader>fg", function()
+    builtin.live_grep({ hidden = true })
+end)
+vim.keymap.set("n", "<leader>/", function()
+    builtin.current_buffer_fuzzy_find({
+        fuzzy = false,
+        case_mode = "ignore_case",
+    })
+end)
+vim.keymap.set("n", "<leader>D", function()
+    builtin.diagnostics({ bufnr = 0 })
+end)
+vim.keymap.set("n", "<leader>da", builtin.diagnostics)
+vim.keymap.set("n", "<leader>dw", function()
+    builtin.diagnostics({
+        severity = vim.diagnostic.severity.WARN,
+    })
+end)
+vim.keymap.set("n", "<leader>de", function()
+    builtin.diagnostics({
+        severity = vim.diagnostic.severity.ERROR,
+    })
+end)
+
+-- resume picker
+vim.keymap.set("n", "<leader>R", ":Telescope resume<cr>")
+
+-- get all pickers
+vim.keymap.set("n", "<leader>g", ":Telescope<cr>")
+
+-- yank history
+vim.keymap.set("n", "<leader>y", ":Telescope yank_history<cr>")
+
+-- dap
+vim.keymap.set("n", "<leader>ds", ":Telescope dap frames<cr>")
+vim.keymap.set("n", "<leader>dc", ":Telescope dap commands<cr>")
+vim.keymap.set("n", "<leader>db", ":Telescope dap list_breakpoints<cr>")
+vim.keymap.set("n", "<leader>dv", ":Telescope dap variables<cr>")
+
+-- harpoon
+vim.keymap.set("n", "<leader>h", ":Telescope harpoon marks<cr>")
