4d7bf8eeaafb92c03c701354df8826079c006de8
brighten <brightenqtompkins@gmail.com>
2024-04-15 09:43:28 -0700

    refactor(neovim): ctrl+j should still accept

--- .config/nvim/lua/plugins/lsp/completion.lua
+++ .config/nvim/lua/plugins/lsp/completion.lua
@@ -15,6 +15,7 @@ cmp.setup({
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-j>"] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({
         {
