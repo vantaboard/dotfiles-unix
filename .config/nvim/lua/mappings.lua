d047bff184f43f8e757a537f5e6d698cb4499b62
Brighten Tompkins <brightenqtompkins@gmail.com>
2025-04-27 17:10:45 -0700

    feat: gitlab merge request mapping

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -157,3 +157,5 @@ vim.keymap.set("n", "<leader>o", ":Octo actions<cr>")
 vim.api.nvim_set_keymap('i', '<F2>', '<cmd>lua require("renamer").rename()<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>rn', '<cmd>lua require("renamer").rename()<cr>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<leader>rn', '<cmd>lua require("renamer").rename()<cr>', { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>mr", function () require"gitlab".choose_merge_request() end)
