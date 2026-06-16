f6effd3a8cd73788ea2d7eaa3335247547fb9da8
Brighten Tompkins <brightenqtompkins@gmail.com>
2026-06-16 13:20:51 -0700

    refactor(desktop): remove i3 and X11 session configs
    Delete i3, picom, autorandr, screenlayout scripts, X session files,
    dmenu_runner, betterlockscreen, i3-cursor-border-state, and udev
    hotplug hooks. Keep i3status config for Waybar py3status.

--- home/dot_screenlayout/main-one-monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --primary --mode 1280x720 --pos 0x360 --rotate normal
--- home/dot_screenlayout/main.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --mode 1920x1080 --pos 3200x0 --rotate normal --output DP-1 --off --output HDMI-0 --primary --mode 1920x1080 --pos 1280x0 --rotate normal --output eDP1 --mode 1280x720 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output VIRTUAL1 --off
